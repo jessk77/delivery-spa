d06a2029ec14652094d01791b05b56a53a318938
Jess Verduzco <jk.do77@gmail.com>
2020-11-15 12:58:27 -0600

    Update firebase.js
    firebase keys

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,11 +1,11 @@
 import * as firebase from "firebase";
 const configOptions = {
-  apiKey: "AIzaSyAuSv7KDHuNFjGem3Aq9r4hBJvNX1Ls4ec",
-    authDomain: "api-project-363815265684.firebaseapp.com",
-    databaseURL: "https://api-project-363815265684.firebaseio.com",
-    projectId: "api-project-363815265684",
-    storageBucket: "api-project-363815265684.appspot.com",
-    messagingSenderId: "363815265684",
-    appId: "1:363815265684:web:d3d498703c3638923d9b81"
+  apiKey: "apiKey",
+    authDomain: "authDomain",
+    databaseURL: "databaseUrl",
+    projectId: "projectID",
+    storageBucket: "Bucket",
+    messagingSenderId: "senderId",
+    appId: "appID"
 };
 firebase.initializeApp(configOptions);
